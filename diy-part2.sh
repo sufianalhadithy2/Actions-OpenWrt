e45ee921e6af6022329b9ae87f1094d3e723c008
sufianalhadithy2 <sufianalhadithy2@gmail.com>
2026-06-09 23:34:34 +0300

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,5 +18,5 @@
 
 # Modify hostname
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
-sed -i '/"u-boot"/,/read-only;/s/read-only;//' target/linux/ath79/dts/ar934x_ubiquiti_xw.dtsi
-sed -i '/"u-boot-env"/,/read-only;/s/read-only;//' target/linux/ath79/dts/ar934x_ubiquiti_xw.dtsi
+#sed -i '/"u-boot"/,/read-only;/s/read-only;//' target/linux/ath79/dts/ar934x_ubiquiti_xw.dtsi
+#sed -i '/"u-boot-env"/,/read-only;/s/read-only;//' target/linux/ath79/dts/ar934x_ubiquiti_xw.dtsi
